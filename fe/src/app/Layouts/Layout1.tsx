c6113e64d0e034118042037c9fbb9128dcf321ae
Pham The Phong <emyeugay12345@gmail.com>
2024-07-28 22:21:01 +0700

    docs:  add code swiper/page.tsx

--- fe/src/app/Layouts/Layout1.tsx
+++ fe/src/app/Layouts/Layout1.tsx
@@ -1,10 +1,12 @@
-import Swiper from "../components/component/swiper/page";
+import Advertisement from "../components/component/swiper/page";
 
 function Layout1({ children }: { children: React.ReactNode }) {
-    return ( <div className="sm:text-[15px] text-[12px]">
-        <Swiper/>
-       {children}
-    </div> );
+  return (
+    <div className="sm:text-[15px] text-[12px]">
+      <Advertisement />
+      {children}
+    </div>
+  );
 }
 
 export default Layout1;
--- fe/src/app/components/component/swiper/page.tsx
+++ fe/src/app/components/component/swiper/page.tsx
@@ -1,7 +1,72 @@
-export default function Swiper() {
-    return (
-        <div>
-            <h1>Swiper</h1>
+// src/app/components/SwiperComponent.tsx
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+import { Navigation, Autoplay } from "swiper/modules";
+import "./styles.scss";
+
+export default function Advertisement() {
+  return (
+    <div className="bg-black flex flex-col sm:flex-row">
+      {/* quảng cáo 1 */}
+      <div className="sm:w-[60vw] w-full h-[300px] bg-slate-400 sm:block hidden">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          modules={[Navigation, Autoplay]}
+          className="mySwiper h-full"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+        </Swiper>
+      </div>
+      {/* quảng cáo 2 + 3 */}
+      <div className="bg-red-600 sm:h-[300px] h-[200px] flex flex-col overflow-hidden">
+        {/* quảng cáo 2 */}
+        <div className="sm:h-[50%] h-[100px] bg-slate-100 overflow-hidden">
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            modules={[Navigation, Autoplay]}
+            className="mySwiper h-full"
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+          </Swiper>
         </div>
-    )
+        {/* quảng cáo 3 */}
+        <div className="sm:h-[50%] h-[100px] bg-[#38d742] overflow-hidden">
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            modules={[Navigation, Autoplay]}
+            className="mySwiper h-full"
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
 }
