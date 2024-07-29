8882bc0ebe34ca8424afee7cdaa008a34352cb94
Pham The Phong <emyeugay12345@gmail.com>
2024-07-29 20:11:29 +0700

    fix: adjust header height, search height, and search result padding
    - Updated header height to improve layout consistency.
    - Modified search input height for better alignment.
    - Adjusted padding for search results to enhance readability.

--- fe/src/app/components/component/header/logo.tsx
+++ fe/src/app/components/component/header/logo.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-} from '@/components/ui/breadcrumb';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 export const Logo = () => {
-    return (
-        <div>
-            <span className="sm:h-[60px] h-[40px] flex items-center">
-                {/* Logo */}
-                <div className="h-full">
-                    <img
-                        src="https://i.pinimg.com/564x/ae/08/0c/ae080c00fe48df5eb9ed6155f853936c.jpg"
-                        alt="logo"
-                    />
-                </div>
-                {/* breadcrumb */}
-                <Breadcrumb className="xl:block hidden ml-10 ">
-                    <BreadcrumbList className="text-white lg:text-[15px] text-[12px]">
-                        <BreadcrumbItem>
+  return (
+    <div>
+      <span className="sm:h-[80px] h-[60px] flex items-center">
+        {/* Logo */}
+        <div className="h-full sm:w-[180px] w-[70px]">
+          <img
+            src="https://i.pinimg.com/564x/ae/08/0c/ae080c00fe48df5eb9ed6155f853936c.jpg"
+            alt="logo"
+          />
+        </div>
+        {/* breadcrumb */}
+        <Breadcrumb className="xl:block hidden ml-10 ">
+          <BreadcrumbList className="text-white lg:text-[15px] text-[12px]">
+            {/* <BreadcrumbItem>
                             <BreadcrumbLink
                                 className=" flex items-center hover:bg-[#a8a8a8] p-4 rounded-sm"
                                 href="/"
@@ -33,10 +33,10 @@ export const Logo = () => {
                                 />
                                 Trang chủ
                             </BreadcrumbLink>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>
-            </span>
-        </div>
-    );
+                        </BreadcrumbItem> */}
+          </BreadcrumbList>
+        </Breadcrumb>
+      </span>
+    </div>
+  );
 };
--- fe/src/app/components/component/header/menu.tsx
+++ fe/src/app/components/component/header/menu.tsx
@@ -1,69 +1,69 @@
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function Menu() {
-    return (
-        <span className="xl:hidden block">
-            <DropdownMenu>
-                <DropdownMenuTrigger className="bg-transparent">
-                    <div className="bg-transparent border-none hover:bg-transparent focus:border-none">
-                        <FontAwesomeIcon
-                            className="text-[#ffffff] sm:size-[30px] size-[20px] ml-5"
-                            icon={faBars}
-                        />
-                    </div>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="sm:w-[200px] w-[150px] sm:text-[18px]">
-                    <DropdownMenuLabel className="sm:text-[18px] hover:bg-transparent">
-                        Danh sách
-                    </DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                        <DropdownMenuItem className="sm:text-[18px] ">
-                            Home
-                        </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                        <DropdownMenuItem className="sm:text-[18px] ">
-                            Giỏ hàng
-                        </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                        <DropdownMenuItem className="sm:text-[18px] ">
-                            Vị trí
-                        </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem className="sm:text-[18px] ">
-                        Chính sách bảo hành
-                    </DropdownMenuItem>
-                    <DropdownMenuGroup>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem className="sm:text-[18px] ">
-                            Phạm Thế Phong
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                    </DropdownMenuGroup>
+  return (
+    <span className="xl:hidden block">
+      <DropdownMenu>
+        <DropdownMenuTrigger className="bg-transparent">
+          <div className="bg-transparent border-none hover:bg-transparent focus:border-none">
+            <FontAwesomeIcon
+              className="text-[#ffffff] sm:size-[30px] size-[30px] ml-5"
+              icon={faBars}
+            />
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="sm:w-[200px] w-[150px] sm:text-[18px]">
+          <DropdownMenuLabel className="sm:text-[18px] hover:bg-transparent">
+            Danh sách
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem className="sm:text-[18px] ">
+              Home
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem className="sm:text-[18px] ">
+              Giỏ hàng
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem className="sm:text-[18px] ">
+              Vị trí
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="sm:text-[18px] ">
+            Chính sách bảo hành
+          </DropdownMenuItem>
+          <DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="sm:text-[18px] ">
+              Phạm Thế Phong
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+          </DropdownMenuGroup>
 
-                    <DropdownMenuItem className="sm:text-[18px] ">
-                        Đăng xuất
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </span>
-    );
+          <DropdownMenuItem className="sm:text-[18px] ">
+            Đăng xuất
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </span>
+  );
 }
 
 export default Menu;
