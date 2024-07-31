bea71ab19596fa991fac199daf36b484223a3081
Pham The Phong <emyeugay12345@gmail.com>
2024-07-31 17:09:15 +0700

    fix: Update card height and add icon

--- fe/src/app/components/component/card/card.tsx
+++ fe/src/app/components/component/card/card.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import { BsCpuFill } from "react-icons/bs";
 
 export default function Card() {
   return (
     <Link href={"/card"}>
       <div className="min-w-[130px] max-w-[300px] hover:shadow-md border-[1px] cursor-pointer">
         <div
-          className="w-full sm:h-[250px] h-[200px] bg-slate-400 bg-cover bg-center"
+          className="w-full  bg-slate-400 bg-cover bg-center"
           style={{
             backgroundImage:
               "url('https://mega.com.vn/media/news/2905_hinh-nen-may-tinh-sieu-de-thuong.jpg')",
@@ -28,12 +29,40 @@ export default function Card() {
           </span>
           <nav className="flex justify-between w-full mt-3">
             <ul>
-              <li>thông số 1</li>
-              <li>thông số 1</li>
+              <li className="flex items-center">
+                <BsCpuFill className="sm:size-[20px] size-[10px] mr-1" />
+                <span className="text-10px">i5 13420H</span>
+              </li>
+              <li className="flex items-center">
+                <div className="sm:size-[20px] size-[10px] mr-1">
+                  <img
+                    src="https://theme.hstatic.net/200000837185/1001221874/14/config_tags_image_2_icon.png?v=1885"
+                    alt=""
+                  />
+                </div>
+                <span className="text-10px">i5 13420H</span>
+              </li>
             </ul>
             <ul>
-              <li>thông số 1</li>
-              <li>thông số 1</li>
+              <li className="flex items-center">
+                <div className="sm:size-[20px] size-[10px] mr-1">
+                  <img
+                    src="//theme.hstatic.net/200000837185/1001221874/14/config_tags_image_4_icon.png?v=1885"
+                    alt=""
+                  />
+                </div>{" "}
+                <span className="text-10px">i5 13420H</span>
+              </li>
+              <li className="flex items-center">
+                {" "}
+                <div className="sm:size-[20px] size-[10px] mr-1">
+                  <img
+                    src="//theme.hstatic.net/200000837185/1001221874/14/config_tags_image_5_icon.png?v=1885"
+                    alt=""
+                  />
+                </div>{" "}
+                <span className="text-10px">i5 13420H</span>
+              </li>
             </ul>
           </nav>
         </div>
