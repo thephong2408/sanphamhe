b15d419ba083f42bdcd659fd3a346c3092ca4bdc
meof792 <minhb8909@gmail.com>
2024-08-07 19:29:51 +0700

    first commit

---
+++ be/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/user', function () {
+//     return view('help');
+// });
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
