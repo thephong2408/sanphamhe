8f2f75a271931a6584a03f52b6bc59fa3d6dd55c
meof792 <136169163+meof792@users.noreply.github.com>
2024-08-08 20:59:08 +0700

    Update api.php

--- be/routes/api.php
+++ be/routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function () {
 //     return view('help');
 // });
-Route::get('/run', [UserController::class, 'run']);
+Route::get('/get-laptops', [UserController::class, 'getLaptops']);
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
