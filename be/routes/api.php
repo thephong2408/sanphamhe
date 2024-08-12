4f6036afa415db1ba5571dcd323f60f90c9ff1eb
meof792 <minhb8909@gmail.com>
2024-08-12 17:58:18 +0700

    config be api

--- be/routes/api.php
+++ be/routes/api.php
@@ -1,13 +1,14 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 
 // Route::get('/user', function () {
 //     return view('help');
 // });
-Route::get('/get-laptops', [UserController::class, 'getLaptops']);
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
+Route::get('/get-carts', [ShopController::class, 'getCarts']);
+Route::get('/get-laptops', [ShopController::class, 'getLaptops']);
+Route::post('/register', [ShopController::class, 'register']);
+Route::post('/login', [ShopController::class, 'login']);
 
