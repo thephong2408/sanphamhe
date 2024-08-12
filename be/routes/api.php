b9a1c5360badefc21fc1f560e8b96890ca2f96bb
meof792 <minhb8909@gmail.com>
2024-08-12 18:50:28 +0700

    add cart api

--- be/routes/api.php
+++ be/routes/api.php
@@ -1,14 +1,19 @@
 <?php
 
-use App\Http\Controllers\ShopController;
+use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
 
 // Route::get('/user', function () {
 //     return view('help');
 // });
-Route::get('/get-carts', [ShopController::class, 'getCarts']);
-Route::get('/get-laptops', [ShopController::class, 'getLaptops']);
-Route::post('/register', [ShopController::class, 'register']);
-Route::post('/login', [ShopController::class, 'login']);
+Route::post('/get-cart', [WebController::class, 'getCart']);
+Route::post('/add-cart', [WebController::class, 'addCart']);
+Route::post('/delete-cart', [WebController::class, 'deleteCart']);
+Route::post('/adjust-cart', [WebController::class, 'adjustCart']);
+Route::post('/delete-one', [WebController::class, 'deleteOne']);
+Route::post('/add-one', [WebController::class, 'addOne']);
+Route::get('/get-laptops', [WebController::class, 'getLaptops']);
+Route::post('/register', [WebController::class, 'register']);
+Route::post('/login', [WebController::class, 'login']);
 
