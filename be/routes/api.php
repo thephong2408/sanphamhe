fedd919047b8306346c15596e465c6e4bcb97822
meof792 <136169163+meof792@users.noreply.github.com>
2024-08-21 08:47:46 +0700

    Update api.php

--- be/routes/api.php
+++ be/routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function () {
 //     return view('help');
 // });
+Route::post('/admin', [WebController::class, 'adminPage']);
 Route::post('/history', [WebController::class, 'history']);
 Route::post('/submit', [WebController::class, 'submit']);
 Route::post('/get-cart', [WebController::class, 'getCart']);
